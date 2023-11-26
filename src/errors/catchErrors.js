9063f2325d548d2482e5a0c37d2ee4fa32cf9d47
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-11-26 17:21:09 +0100

    custom error handling

---
+++ src/errors/catchErrors.js
@@ -0,0 +1,12 @@
+function useCatchErrors(fn) {
+    return async (req, res, next) => {
+      try {
+        await fn(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    };
+  }
+  
+  module.exports = useCatchErrors;
+  
